3080c5ce607f8f9734064f7881b70502b17c599a
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-17 10:11:58 -0700

    Added Check If Prime

--- PrimeTime.cpp
+++ PrimeTime.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+bool checkIfPrime(int number) {
+    bool ifPrime = true;
+
+    for (int a = number - 1; a > 1; a--)
+    {
+        if (number % a != 0)
+            ifPrime = false;
+    }
+
+    return ifPrime;
+}
+
+
+
 int main()
 {
     std::cout << "Hello World!\n";
