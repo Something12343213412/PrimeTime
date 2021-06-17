79fad51e397552f9c25e14c4f4e545715248b3ad
Kevin Sandberg <Kevbo2007@icloud.com>
2021-06-17 10:35:49 -0700

    Emptied the main function and changed check if prime to check if a number is prime recursively.

--- PrimeTime.cpp
+++ PrimeTime.cpp
@@ -2,28 +2,21 @@
 
 using namespace std;
 
-bool checkIfPrime(int number) {
-    bool ifPrime = true;
+bool checkIfPrime(int number, int divider) {
+    if (divider == 1)
+        return true;
 
-    for (int a = number - 1; a > 1; a--)
-    {
-        if (number % a == 0)
-            ifPrime = false;
+    if (number % divider == 0)
+        checkIfPrime(number, divider - 1);
+
+    else {
+        return false;
     }
-
-    return ifPrime;
 }
 
 
 
 int main()
 {
-    int b = 2;
-    while (true) {
-        if (checkIfPrime(b))
-            cout << b << endl;
-        b++;
-        if (b > 1000)
-            break;
-    }
+    
 }
